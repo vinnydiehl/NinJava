19460f0e5e3b2d9bc7b5edd804510637b82ac42c
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-11-04 13:20:01 -0500

    Improve Num.dateString
    Its output was previously more or less illegible, now it gives a nice
    human readable date. Also, of course, added documentation.

--- src/Num.java
+++ src/Num.java
@@ -1,7 +1,15 @@
-public class Num{
+public class Num {
     //This class has stuff to do with
     //Numbers such as date, ordinalization,
     //etc.
+
+    public static final String[] MONTHS = {
+        null,
+        "January", "Febuary", "March", "April",
+        "May", "June", "July", "August",
+        "September", "October", "November", "December"
+    };
+
     /**
      *Ordinalizes a number
      *EX: 1 => 1st
@@ -16,14 +24,26 @@ public class Num{
     }
 
     /**
-     *Converts a date
-     *EX: 12,31,2012 => Decemeber 31st 2012
+     * Creates a human readable date out of the month, day, and year numbers.
      *
+     * <pre>
+     * {@code
+     * Num.dateString(3, 4, 2012)   // =&gt; "March 4th, 2012"
+     * Num.dateString(12, 1, 2012)  // =&gt; "December 1st, 2012"
+     * }
+     * </pre>
      *
+     * @param month the number of the month
+     * @param day the day of the month
+     * @param year the year
+     * @return a human readable date formed from the number inputs
      */
-    public String DateCon(int month, int day, int year){
-        String months[] = {null,"January","Febuary","March","April","May","June","July","August","September","October", "November","December"};
-        return months[month]+ordinalize(day)+String.valueOf(year);
+    public static String dateString(int month, int day, int year) {
+        return String.format(
+            "%s %s, %d",
+            MONTHS[month],
+            ordinalize(day),
+            year
+        );
     }
-
-    }
+}
